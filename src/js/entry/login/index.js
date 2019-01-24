52881970045c898950db35d7ede22b7df2ecd020
申朝 <zhao.shen@tcredit.com>
2019-01-24 14:42:51 +0800

    feat: modify

--- src/js/entry/login/index.js
+++ src/js/entry/login/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Vant from 'vant';
-import Test from './test';
+import Login from './login';
 
 Vue.use(Vant);
 
 new Vue({
     el: '#app',
-    components: {Test},
-    template: '<Test/>'
+    components: {Login},
+    template: '<Login/>'
 });
